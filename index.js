a6f5d672582b331d44dcb86d5a731039c5880d6c
george barreira <barreirageorge@gmail.com>
2022-10-18 20:54:43 -0300

    usamos o insomnia rest no arq. index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 const hostname = '192.168.3.54'
-const port = 5000
+const port = 5001
 
 const books = [
 
@@ -11,8 +11,6 @@ const books = [
     { _id: 3, title: 'A Casa', autor: 'Raquel de Queiroz', favorito: false },
 
 ]
-
-
 
 app.use(
 
@@ -34,10 +32,35 @@ app.get('/html', (req, res) => {
 
     res.sendFile(`${__dirname}/index.html`)
 })
+
 //filter i.favorito = true / !i.favorito = false
 app.get('/books', (req, res) => {
-res.send(books.filter(i => i.favorito))
+
+    res.send(books.filter(i => i.favorito))
 })
+
+app.post('/books', (req, res) => {
+
+    books.push(req.body)
+
+    res.send(req.body)
+
+})
+
+app.get('/books/:id', (req, res) => {
+
+    //console.log(req.params.id)
+
+    const book = books.find(
+        (book) => book._id === req.params.id
+    )
+
+    if (book)
+        res.send(book)
+    else
+        res.sendStatus(404)
+})
+
 
 app.listen(
     port,
