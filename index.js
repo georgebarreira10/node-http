037819c8581d64c24fb4e1824b41c8bc011e042a
george barreira <barreirageorge@gmail.com>
2022-10-25 20:42:28 -0300

    app.patch e app.delete

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const books = [
     { _id: 1, title: 'O Dia do Coringa', autor: 'Jostein', favorito: true },
     { _id: 2, title: 'O mundo de Sofia', autor: 'Jostein', favorito: true },
     { _id: 3, title: 'A Casa', autor: 'Raquel de Queiroz', favorito: false },
-    
+
 ]
 
 app.use(
@@ -41,15 +41,15 @@ app.get('/books', (req, res) => {
 })
 
 app.post('/books', (req, res) => {
-    
+
     //array de objeto. o body contém a lista que é enviada
     const body = req.body
-    
+
     console.log('body', body)
 
     //percorrer a lista (sacola)
     body.map(obj => books.push(obj))
-    
+
     //push para arrays
     //books.push(req.body)
 
@@ -72,13 +72,37 @@ app.get('/books/:id', (req, res) => {
 })
 
 app.delete('/books/:id', (req, res) => {
-let id = req.params.id
-let index = books.findIndex(obj => obj._id === id)
 
-books.splice(index, 1)
+    //'obj._id' é inteiro e '== id' é string relacionado ao req.params.id 
+    //outraforma: '=== parseInt(id)'. dar preferencia para === estritamente igual
 
-res.send(books)
+    let id = req.params.id
+    let index = books.findIndex(obj => obj._id === parseInt(id))
 
+    console.log('index', index)
+
+    books.splice(index, 1)
+
+    res.status(200).json(books)
+
+
+})
+
+app.patch('/books/:id', (req, res) => {
+
+    let id = parseInt(req.params.id)
+
+    //buscar livro. checar se ele existe.
+    let book = books.find(obj => obj._id === id)
+    console.log('book:', book)
+    
+    //mudança do title do livro
+    let body = req.body
+    console.log('body:', body)
+
+    book.title = body.title
+    book.autor = body.autor
+    res.send(books)
 
 })
 
